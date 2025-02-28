e74d84b930fa16ad3a49e9e5ad850d445a04e2a6
Witcher <Witcher9990610@gmail.com>
2025-02-28 10:36:24 +0200

    Add files via upload

---
+++ resume.cpp
@@ -0,0 +1,29 @@
+// Present: Misha Nazarchuk
+// Date: 25/02/2025
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    cout <<"MISHA NAZARCHUK\n" ;
+    cout << "\tContact information:\n"
+            "Email: Witcher9990610@gmail.com\n"
+            "Github:https://github.com/Witcher60\n"
+            "Telegram: @Witcher9990610\n";
+    cout << "\tSkills:\n"
+            "Python\n"
+            "HTML\n"
+            "CSS\n"
+            "\tPrograms:\n"
+            "Visual Studio Code\n"
+            "Blender\n"
+            "Unreal Engine\n";
+    cout << "\tLanguages:\n"
+            "Ukrainian-native\n"
+            "Russian-native\n"
+            "English-А2\n";
+    cout << "\tAbout me:\n"
+            "Hi, I am a beginner programmer, I develop in such areas as software development, 3D modeling, animation and Game Development.\n"
+            "I am a student of the IT Step Academy.\n";
+}
